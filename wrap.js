9a6a5d476c6f31f169e41988819102b492a2c99b
2betop <liaoxuezhi@icloud.com>
2015-11-21 17:46:34 +0800

    fix sourcemap

--- wrap.js
+++ wrap.js
@@ -27,14 +27,47 @@ module.exports = function(file, opts) {
       deps = ' [' + reqs.join(', ') + '],';
     }
 
+    var originContent = content;
+
     if (opts.tab) {
       content = fis.util.pad(' ', opts.tab) + content.split(/\n|\r\n|\r/g).join('\n' + fis.util.pad(' ', opts.tab));
     }
 
-    content = 'define(\'' + (file.moduleId || file.id) + '\',' + deps + ' function(require, exports, module) {\n\n' + content + '\n\n});\n';
+    var prefix = 'define(\'' + (file.moduleId || file.id) + '\',' + deps + ' function(require, exports, module) {\n\n';
+    var affix = '\n\n});\n';
+
+    content = prefix + content + affix;
 
     if (file.moduleId !== file.id) {
       file.extras.moduleId = file.moduleId;
+    }
+
+    // 同时修改 sourcemap 文件内容。
+    var derived = file.derived;
+    if (!derived || !derived.length) {
+      derived = file.extras && file.extras.derived;
+    }
+
+    if (derived && derived[0] && derived[0].rExt === '.map') {
+      var SourceMap = require('source-map');
+      
+
+      var sourcemap = derived[0];
+      var json = JSON.parse(sourcemap.getContent());
+      var smc = new SourceMap.SourceMapConsumer(json);
+
+      var sourceNode = new SourceMap.SourceNode();
+
+      sourceNode.add(prefix);
+      sourceNode.add(SourceMap.SourceNode.fromStringWithSourceMap(originContent, smc));
+      sourceNode.add(affix);
+
+      var code_map = sourceNode.toStringWithSourceMap({
+        file: smc.file
+      });
+
+      var generater = SourceMap.SourceMapGenerator.fromSourceMap(new SourceMap.SourceMapConsumer(code_map.map.toJSON()));
+      sourcemap.setContent(generater.toString());
     }
 
     file.setContent(content);
